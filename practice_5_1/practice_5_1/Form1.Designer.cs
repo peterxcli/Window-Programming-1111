fbc366a0189f7a50fc4c90e808315508893abee6
peterxcli <peterxcli@gmail.com>
2022-10-15 00:42:57 +0800

    practice_5_1 finish but uncheck

---
+++ practice_5_1/practice_5_1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace practice_5_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputWord = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.displayAnswer = new System.Windows.Forms.Label();
+            this.displayErrorTime = new System.Windows.Forms.Label();
+            this.displayTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(314, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(314, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "請輸入要猜的英文單字:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // inputWord
+            // 
+            this.inputWord.Location = new System.Drawing.Point(334, 227);
+            this.inputWord.Name = "inputWord";
+            this.inputWord.Size = new System.Drawing.Size(139, 22);
+            this.inputWord.TabIndex = 2;
+            this.inputWord.TextChanged += new System.EventHandler(this.inputWord_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(334, 289);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(138, 66);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // displayAnswer
+            // 
+            this.displayAnswer.Location = new System.Drawing.Point(21, 289);
+            this.displayAnswer.Name = "displayAnswer";
+            this.displayAnswer.Size = new System.Drawing.Size(776, 68);
+            this.displayAnswer.TabIndex = 4;
+            this.displayAnswer.Text = "label3";
+            this.displayAnswer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // displayErrorTime
+            // 
+            this.displayErrorTime.AutoSize = true;
+            this.displayErrorTime.Location = new System.Drawing.Point(663, 117);
+            this.displayErrorTime.Name = "displayErrorTime";
+            this.displayErrorTime.Size = new System.Drawing.Size(33, 12);
+            this.displayErrorTime.TabIndex = 5;
+            this.displayErrorTime.Text = "label4";
+            // 
+            // displayTime
+            // 
+            this.displayTime.AutoSize = true;
+            this.displayTime.Location = new System.Drawing.Point(665, 75);
+            this.displayTime.Name = "displayTime";
+            this.displayTime.Size = new System.Drawing.Size(33, 12);
+            this.displayTime.TabIndex = 6;
+            this.displayTime.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayTime);
+            this.Controls.Add(this.displayErrorTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.inputWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayAnswer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputWord;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label displayAnswer;
+        private System.Windows.Forms.Label displayErrorTime;
+        private System.Windows.Forms.Label displayTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ practice_5_1/practice_5_1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice_5_1
+{
+    public partial class Form1 : Form
+    {
+        Button[] btnKeys;
+        string word = "";
+        int timeCounter = 0, errorCounter = 0;
+        Dictionary<string, int> dict;
+        public Form1()
+        {
+            InitializeComponent();
+            btnKeysInit();
+            timer1.Interval = 1000;
+            dict = new Dictionary<string, int>();
+            timer1.Tick += (s, e) => timer1_Tick(s, e);
+        }
+        void btnKeysInit()
+        {
+            btnKeys = new Button[26];
+            for (int i = 0; i < 26; i++)
+            {
+                btnKeys[i] = new Button();
+                int idx = i;
+                btnKeys[i].Click += (s, e) => OnBtnKeysClick(idx);
+                btnKeys[i].KeyPress += (s, e) => OnKeyPress(s, e);
+                this.Controls.Add(btnKeys[i]);
+            }
+        }
+        void OnBtnKeysClick(int idx)
+        {
+            
+        }
+        void renderMenu()
+        {
+            dict.Clear();
+            word = "";
+            errorCounter = timeCounter = 0;
+
+            label1.Visible = true;
+            label2.Visible = true;
+            btnStart.Visible = true;
+            inputWord.Visible = true;
+            
+
+            for (int i = 0; i < 26; i++) btnKeys[i].Visible = false;
+            displayErrorTime.Visible = false;
+            displayTime.Visible = false;
+            displayAnswer.Visible = false;
+
+            btnStart.Enabled = true;
+            inputWord.Enabled = true;
+
+            label1.Text = "猜英文單字\n";
+            label1.Text += "6次猜錯機會";
+            label1.Font = new Font("標楷體", 20, FontStyle.Bold);
+            inputWord.Text = "";
+
+        }
+
+        void renderPlayground()
+        {
+            for (int i = 0; i < 26; i++) btnKeys[i].Visible = true;
+            for (int i = 0; i < 26; i++) btnKeys[i].BackColor = Color.White;
+            displayErrorTime.Visible = true;
+            displayTime.Visible = true;
+            displayAnswer.Visible = true;
+
+            label1.Visible = false;
+            label2.Visible = false;
+            btnStart.Visible = false;
+            inputWord.Visible = false;
+
+            btnStart.Enabled = false;
+            inputWord.Enabled = false;
+
+            char c = 'A';
+            for (int i = 0; i < 26; i++, c++)
+            {
+                btnKeys[i].SetBounds(50 * (i % 10) + 150 + (i/10 == 2? 100:0), 50 * (i / 10) + 60, 40, 40);
+                btnKeys[i].Text = c.ToString();
+                btnKeys[i].Font = new Font("Arial", 12, FontStyle.Bold);
+            }
+            
+            timer1.Enabled = true;
+            displayTime.Text = "時間: " + timeCounter.ToString();
+            displayErrorTime.Text = "猜錯次數: " + errorCounter.ToString();
+            showResult();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            renderMenu();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            word = inputWord.Text;
+            if (word == "") return;
+            word = word.ToUpper();
+            renderPlayground();
+        }
+
+        private void inputWord_TextChanged(object sender, EventArgs e)
+        {
+            string tmp = inputWord.Text;
+            tmp = Regex.Replace(tmp, "[^a-zA-Z]+", "");
+            inputWord.Text = tmp;
+            inputWord.SelectionStart = tmp.Length;
+            inputWord.SelectionLength = 0;
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().Length != Regex.Replace(e.KeyChar.ToString(), "[^a-zA-Z]+", "").Length) return;
+            e.KeyChar.ToString();
+            processAfterGuess(e.KeyChar.ToString().ToUpper());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeCounter++;
+            displayTime.Text = "時間: " + timeCounter.ToString();
+        }
+
+        void processAfterGuess(string ch)
+        {
+            if (dict.ContainsKey(ch)) return;
+
+            dict.Add(ch.ToString(), 1);
+
+            if (word.IndexOf(ch.ToString()) == -1)
+            {
+                btnKeys[char.Parse(ch) - 'A'].Visible = false;
+                btnKeys[char.Parse(ch) - 'A'].Enabled = false;
+                errorCounter++;
+                /*
+                todo: guess wrong char
+                */
+            }
+            else
+            {
+                btnKeys[char.Parse(ch) - 'A'].BackColor = Color.LightGreen;
+            }
+            showResult();
+
+        }
+
+        void showResult()
+        {
+            displayAnswer.Text = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (dict.ContainsKey(word[i].ToString())) displayAnswer.Text += word[i];
+                else displayAnswer.Text += "_";
+                displayAnswer.Text += (i == word.Length - 1 ? "" : " ");
+            }
+            displayErrorTime.Text = "猜錯次數: " + errorCounter.ToString();
+            if (isLose())
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("You Lose !");
+                renderMenu();
+            }
+            else if (isWin())
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("花費時間: " + timeCounter + "\n" + "猜錯 " + errorCounter + "次" , "You win !");
+                renderMenu();
+            }
+        }
+
+        bool isWin()
+        {
+            foreach(var item in word) if (!dict.ContainsKey(item.ToString())) return false;
+            return true;
+        }
+
+        bool isLose()
+        {
+            return errorCounter >= 6;
+        }
+    }
+}
